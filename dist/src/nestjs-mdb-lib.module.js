aeed8ca8b7c672ed62629bb1378b49ecfaed943d
v_jianjpeng(彭涧) <v_jianjpeng@wesure.cn>
2020-08-19 16:23:18 +0800

    perf: build

--- dist/src/nestjs-mdb-lib.module.js
+++ dist/src/nestjs-mdb-lib.module.js
@@ -5,12 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var NestjsMdbLibModule_1;
 Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@nestjs/common");
 const nestjs_mdb_lib_service_1 = require("./nestjs-mdb-lib.service");
-let NestjsMdbLibModule = class NestjsMdbLibModule {
+const common_1 = require("@nestjs/common");
+const constants_1 = require("./constants");
+let NestjsMdbLibModule = NestjsMdbLibModule_1 = class NestjsMdbLibModule {
+    static register(options) {
+        return {
+            module: NestjsMdbLibModule_1,
+            providers: [
+                {
+                    provide: constants_1.MDB_OPTIONS,
+                    useValue: options,
+                },
+                nestjs_mdb_lib_service_1.NestjsMdbLibService
+            ],
+            exports: [nestjs_mdb_lib_service_1.NestjsMdbLibService],
+        };
+    }
 };
-NestjsMdbLibModule = __decorate([
+NestjsMdbLibModule = NestjsMdbLibModule_1 = __decorate([
     common_1.Module({
         providers: [nestjs_mdb_lib_service_1.NestjsMdbLibService],
         exports: [nestjs_mdb_lib_service_1.NestjsMdbLibService],
