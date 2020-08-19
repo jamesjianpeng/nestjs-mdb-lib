b3cbe594d3921d03eb207433a81bd921a84ce539
v_jianjpeng(彭涧) <v_jianjpeng@wesure.cn>
2020-08-19 16:01:20 +0800

    perf: 重新 build

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,2 +1,7 @@
-export * from './nestjs-mdb-lib.module';
-export * from './nestjs-mdb-lib.service';
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./nestjs-mdb-lib.module"));
+__export(require("./nestjs-mdb-lib.service"));
--- dist/src/nestjs-mdb-lib.module.js
+++ dist/src/nestjs-mdb-lib.module.js
@@ -1,17 +1,19 @@
+"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { Module } from '@nestjs/common';
-import { NestjsMdbLibService } from './nestjs-mdb-lib.service';
+Object.defineProperty(exports, "__esModule", { value: true });
+const common_1 = require("@nestjs/common");
+const nestjs_mdb_lib_service_1 = require("./nestjs-mdb-lib.service");
 let NestjsMdbLibModule = class NestjsMdbLibModule {
 };
 NestjsMdbLibModule = __decorate([
-    Module({
-        providers: [NestjsMdbLibService],
-        exports: [NestjsMdbLibService],
+    common_1.Module({
+        providers: [nestjs_mdb_lib_service_1.NestjsMdbLibService],
+        exports: [nestjs_mdb_lib_service_1.NestjsMdbLibService],
     })
 ], NestjsMdbLibModule);
-export { NestjsMdbLibModule };
+exports.NestjsMdbLibModule = NestjsMdbLibModule;
