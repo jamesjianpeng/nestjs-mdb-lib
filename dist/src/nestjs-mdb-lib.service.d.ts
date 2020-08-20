f830fab10f5e9997354a9d564e9602ff491823fd
jamesjianpeng <jamesjianpeng@gmail.com>
2020-08-20 22:55:58 +0800

    build

--- dist/src/nestjs-mdb-lib.service.d.ts
+++ dist/src/nestjs-mdb-lib.service.d.ts
@@ -1,12 +1,12 @@
 import { OnModuleInit } from '@nestjs/common';
 import { MongoClient, Db, Collection } from 'mongodb';
-import { IMdbOptions, IColOption } from './interface';
+import { IMdbOptions, IMdb, IColOption } from './interface';
 export declare class NestjsMdbLibService implements OnModuleInit {
     private options;
     private dbMap;
     private cliMap;
     constructor(options: IMdbOptions);
-    onModuleInit(): IMdbOptions;
+    onModuleInit(): IMdb[];
     test(): Promise<string>;
     getClis(): Promise<void>;
     getCli(url: any): Promise<MongoClient>;
